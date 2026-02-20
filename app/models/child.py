7d9f855916f5ff9539e6697142292dc2ade831d3
Tomlena-tech <decourttthomas@gmail.com>
2026-02-20 14:24:58 +0100

    fix: add GPSHistory import in child model

--- app/models/child.py
+++ app/models/child.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.core.database import Base
 from datetime import datetime
+from app.models.gps_history import GPSHistory
 
 
 class Child(Base):
